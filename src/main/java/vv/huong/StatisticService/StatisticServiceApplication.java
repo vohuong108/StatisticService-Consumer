aa7d5989b2b110894f3c653bd3f4f656ea959506
vohuong108 <huongvovan123@gmail.com>
2022-12-02 09:49:54 +0700

    handle error in consumer by blocking, non-blocking mechanism

--- src/main/java/vv/huong/StatisticService/StatisticServiceApplication.java
+++ src/main/java/vv/huong/StatisticService/StatisticServiceApplication.java
@@ -1,9 +1,15 @@
 package vv.huong.StatisticService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.converter.JsonMessageConverter;
+import org.springframework.util.backoff.FixedBackOff;
 
 @SpringBootApplication
 public class StatisticServiceApplication {
@@ -16,4 +22,9 @@ public class StatisticServiceApplication {
 	JsonMessageConverter converter() {
 		return new JsonMessageConverter();
 	}
+
+//	@Bean
+//	DefaultErrorHandler errorHandler(KafkaTemplate<String, Object> template) {
+//		return new DefaultErrorHandler(new DeadLetterPublishingRecoverer(template), new FixedBackOff(1000L, 2));
+//	}
 }
--- src/main/java/vv/huong/StatisticService/service/StatisticService.java
+++ src/main/java/vv/huong/StatisticService/service/StatisticService.java
@@ -3,6 +3,8 @@ package vv.huong.StatisticService.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 import vv.huong.StatisticService.model.Statistic;
 import vv.huong.StatisticService.repository.StatisticRepo;
@@ -16,6 +18,22 @@ public class StatisticService {
     @KafkaListener(id = "statisticGroup", topics = "statistic")
     public void listen(Statistic statistic) {
         log.info("Received: {}", statistic.getMessage());
+//        statisticRepo.save(statistic);
+        throw new RuntimeException("Error DLT");
+    }
+
+//    @KafkaListener(id = "dltGroup", topics = "statistic.DLT")
+//    public void dltListen(Statistic statistic) {
+//        log.info("Received statistic.DLT: {}", statistic.getMessage());
+//        statisticRepo.save(statistic);
+//    }
+
+    @RetryableTopic(attempts = "5", dltTopicSuffix = "-dlt",
+        backoff = @Backoff(delay = 2_000, multiplier = 2)
+    )
+    @KafkaListener(id = "dltGroup", topics = "statistic.DLT")
+    public void dltListen(Statistic statistic) {
+        log.info("Received statistic.DLT: {}", statistic.getMessage());
         statisticRepo.save(statistic);
     }
 }
